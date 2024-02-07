67fb72bd0863e70e18814549a97ac8b4fa2cae4b
JrzenonDev <jrobertoonb@gmail.com>
2024-02-06 21:19:25 -0300

    feat: passing NoteCard props

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,11 @@ import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
 export function App() {
+  const note = {
+    date: new Date(),
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit ullam odio saepe, omnis perferendis accusamus commodi velit sit cumque quasi aut dolor, quis ipsam porro odit deleniti itaque hic at.",
+  };
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="logo" />
@@ -19,11 +24,8 @@ export function App() {
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
 
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note} />
+        <NoteCard note={note} />
       </div>
     </div>
   );
